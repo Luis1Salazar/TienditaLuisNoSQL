5377a1576df76a43b64df767d46c131892f6bb5c
Luis Salazar <bayernsito@hotmail.com>
2022-03-12 19:03:37 -0500

    Creacion

---
+++ configuracion.js
@@ -0,0 +1,5 @@
+const config = {
+    DB_URL: 'mongodb+srv://luissalazar:ups1234@cluster0.6fymk.mongodb.net/tienditaplatweb?retryWrites=true&w=majority',
+    PUERTO: process.env.PORT || 3000
+}
+module.exports = config
